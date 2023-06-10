34ca55ceab8a264b3f6af3b24d06695f07773328
James <jamesdev4you@gmail.com>
2023-06-10 12:49:15 -0400

    menu w/ logo, theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Menu from "./primary_component/Menu";
+
+const theme = createTheme({
+  palette: {
+    primary: { main: "#000000" },
+    typography: { main: "#FFFFFF" },
+    error: { main: "#ffd700" },
+  },
+});
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Menu />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/primary_component/Menu.js
@@ -0,0 +1,102 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Logo from "../assets/logo.png";
+
+const drawerWidth = 240;
+const navItems = ["Home", "About", "Services", "Testimonials", "Contact"];
+
+export default function Menu() {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box
+      onClick={handleDrawerToggle}
+      sx={{ textAlign: "center", backgroundColor: "primary.main" }}
+    >
+      <img src={Logo} style={{ height: "75px", width: "150px" }} />
+      <Divider sx={{ backgroundColor: "primary.main" }} />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center", color: "#fff" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <img
+            src={Logo}
+            style={{
+              display: { xs: "none", sm: "block" },
+              height: "70px",
+              width: "150px",
+            }}
+          />
+          <Box
+            sx={{ display: { xs: "none", sm: "block" }, marginLeft: "auto" }}
+          >
+            {navItems.map((item) => (
+              <Button key={item} sx={{ paddingLeft: "25px", color: "#fff" }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+              backgroundColor: "primary.main",
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+}
