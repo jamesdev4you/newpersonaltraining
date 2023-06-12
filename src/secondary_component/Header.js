71682d12d43eea150d9518e5570547501ab85eac
James <jamesdev4you@gmail.com>
2023-06-12 12:44:53 -0400

    desktop MVP homepage - still need 2 sections

--- src/secondary_component/Header.js
+++ src/secondary_component/Header.js
@@ -3,13 +3,17 @@ import Box from "@mui/material/Box";
 import { Button } from "../custom_components/Styled";
 import Header2 from "../assets/header.jpg";
 import Typography from "@mui/material/Typography";
-import '../index.css';
-
-
+import "../index.css";
 
 export default function Header() {
   return (
-    <Box sx={{ display: "flex", width: "100%", height: "800px" }}>
+    <Box
+      sx={{
+        display: "flex",
+        width: "100%",
+        height: { xl: "100vh", lg: "800px" },
+      }}
+    >
       <Box
         sx={{
           width: "50%",
@@ -24,27 +28,41 @@ export default function Header() {
       >
         <Typography
           variant="h3"
-          sx={{ fontFamily: 'Ubuntu', width: "440px", alignText: "left", color: "error.main", fontSize: {lg: '40px', sm: '24px'} }}
+          sx={{
+            fontFamily: "Ubuntu",
+            width: { xl: "100%", lg: "440px" },
+            alignText: "left",
+            color: "error.main",
+            fontSize: { xl: "68px", lg: "40px", sm: "24px" },
+          }}
         >
           Transform yourself
         </Typography>
         <Typography
           variant="h3"
           sx={{
-            fontFamily: 'Ubuntu',
-            width: "440px",
+            fontFamily: "Ubuntu",
+            width: { xl: "100%", lg: "440px" },
             alignText: "left",
             color: "#fff",
-            fontSize: {lg: '40px', sm: '24px'}
+            fontSize: { xl: "68px", lg: "40px", sm: "24px" },
           }}
         >
           into the Perfect you
         </Typography>
-        <Typography variant="h6" sx={{ fontFamily: 'Ubuntu', width:{lg: '380px', sm: '150px'}, color: "#fff", fontSize: {lg: '16px', sm: '10px'} }}>
+        <Typography
+          variant="h6"
+          sx={{
+            fontFamily: "Ubuntu",
+            width: { xl: "60%", lg: "380px", sm: "150px" },
+            color: "#fff",
+            fontSize: { xl: "28px", lg: "16px", sm: "10px" },
+          }}
+        >
           Providing services in Yoga, Body Building, Personal Training, and
           Nutrition
         </Typography>
-        <Button variant="outlined" pill >
+        <Button variant="outlined" pill>
           Learn more
         </Button>
       </Box>
@@ -55,8 +73,8 @@ export default function Header() {
           backgroundImage: `url(${Header2})`,
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
-          backgroundPosition: 'right',
-          filter: 'grayscale()'
+          backgroundPosition: "right",
+          filter: "grayscale()",
         }}
       ></Box>
     </Box>
--- src/secondary_component/HomeTestimonial.js
+++ src/secondary_component/HomeTestimonial.js
@@ -2,11 +2,11 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import PersonalTraining from "../assets/personaltraining.jpg";
-import '../index.css';
+import "../index.css";
 
 const HomeTestimonial = () => {
   return (
-    <Box sx={{ height: "1350px", width: "100%" }}>
+    <Box sx={{ height: "1500px", width: "100%" }}>
       <Box
         sx={{
           width: "70%",
@@ -23,11 +23,16 @@ const HomeTestimonial = () => {
             flexDirection: "column",
             alignItems: "center",
             gap: "50px",
-            marginBottom: '50px'
+            marginBottom: "50px",
           }}
         >
-          <Typography variant="h3" sx={{fontFamily: "Ubuntu", color: "error.dark"}}>The Results You Can Expect</Typography>
-          <Typography variant="h5" sx={{fontFamily: "Normal"}}>
+          <Typography
+            variant="h3"
+            sx={{ fontFamily: "Ubuntu", color: "error.dark" }}
+          >
+            The Results You Can Expect
+          </Typography>
+          <Typography variant="h5" sx={{ fontFamily: "Normal" }}>
             The Armoury would be nothing without the men and women that have put
             their trust in us over the last 10 years, and working together we've
             had the privilege of seeing some truly tremendous accomplishments.
@@ -38,154 +43,314 @@ const HomeTestimonial = () => {
             through the effort they've put in.{" "}
           </Typography>
         </Box>
-        <Box    
-        sx={{
+        <Box
+          sx={{
             display: "flex",
-            width: '100%',
+            width: "100%",
             flexDirection: "row",
             alignItems: "center",
             justifyContent: "center",
             gap: "30px",
-            
-          }}>
-          <Box    
-        sx={{
-            display: "flex",
-            width: '50%',
-            height: '300px',
-            flexDirection: "row",
-            alignItems: "start",
-            justifyContent: "space-between",
-            gap: "30px",
-            
-          }}>
-            
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              width: "50%",
+              height: { xl: "500px", lg: "300px" },
+              flexDirection: "row",
+              alignItems: "start",
+              justifyContent: "space-between",
+              gap: "30px",
+            }}
+          >
             <Box
-          sx={{
-            width: "47%",
-            height: "300px",
-            backgroundImage: `url(${PersonalTraining})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            backgroundPosition: "right"
-          }}
-        ></Box>
+              sx={{
+                width: "47%",
+                height: { xl: "500px", lg: "300px" },
+                backgroundImage: `url(${PersonalTraining})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "right",
+              }}
+            ></Box>
+            <Box
+              sx={{
+                width: "47%",
+                height: { xl: "500px", lg: "300px" },
+                backgroundImage: `url(${PersonalTraining})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "right",
+              }}
+            ></Box>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              width: "50%",
+              flexDirection: "row",
+              alignItems: "start",
+              justifyContent: "center",
+              gap: "30px",
+
+              height: { xl: "500px", lg: "300px" },
+            }}
+          >
+            <Box
+              sx={{
+                width: "47%",
+                height: { xl: "500px", lg: "300px" },
+                backgroundImage: `url(${PersonalTraining})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "right",
+              }}
+            ></Box>
+            <Box
+              sx={{
+                width: "47%",
+                height: { xl: "500px", lg: "300px" },
+                backgroundImage: `url(${PersonalTraining})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "right",
+              }}
+            ></Box>
+          </Box>
+        </Box>
         <Box
           sx={{
-            width: "47%",
-            height: "300px",
-            backgroundImage: `url(${PersonalTraining})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            backgroundPosition: "right"
-          }}
-        ></Box>
-        
-            </Box>
-      <Box    
-        sx={{
             display: "flex",
-            width: '50%',
+            width: "100%",
+            height: "250px",
             flexDirection: "row",
-            alignItems: "start",
-            justifyContent: "center",
+            alignItems: "center",
+            justifyContent: "space-between",
             gap: "30px",
-            
-            height: '300px',
-          }}><Box
-          sx={{
-            width: "47%",
-            height: "300px",
-            backgroundImage: `url(${PersonalTraining})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            backgroundPosition: "right"
+            backgroundColor: "white",
           }}
-        ></Box>
+        >
+          <Box
+            sx={{
+              width: "48.4%",
+              height: "180px",
+              padding: "10px",
+              backgroundColor: "white",
+              border: "1px solid black",
+              borderRadius: "15px",
+              boxShadow:
+                "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "start",
+              flexDirection: "column",
+              gap: "5px",
+            }}
+          >
+            <Typography sx={{ fontSize: "18px", fontFamily: "Ubuntu" }}>
+              {" "}
+              ❝ Just wanted to share a quick note and let you know that you guys
+              do a really good job. I'm glad I decided to work with you. I never
+              have any problem at all. ❞
+            </Typography>
+            <Typography sx={{ fontSize: "18px", fontFamily: "Ubuntu" }}>
+              {" "}
+              - James Boyle
+            </Typography>
+            <Typography sx={{ fontSize: "18px", fontFamily: "Ubuntu" }}>
+              {" "}
+              ⭐⭐⭐⭐⭐
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              width: "48.4%",
+              height: "180px",
+              padding: "10px",
+              backgroundColor: "white",
+              border: "1px solid black",
+              borderRadius: "15px",
+              boxShadow:
+                "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "start",
+              flexDirection: "column",
+              gap: "5px",
+            }}
+          >
+            <Typography sx={{ fontSize: "18px", fontFamily: "Ubuntu" }}>
+              {" "}
+              ❝ Just wanted to share a quick note and let you know that you guys
+              do a really good job. I'm glad I decided to work with you. I never
+              have any problem at all. ❞
+            </Typography>
+            <Typography sx={{ fontSize: "18px", fontFamily: "Ubuntu" }}>
+              {" "}
+              - James Boyle
+            </Typography>
+            <Typography sx={{ fontSize: "18px", fontFamily: "Ubuntu" }}>
+              {" "}
+              ⭐⭐⭐⭐⭐
+            </Typography>
+          </Box>
+        </Box>
         <Box
           sx={{
-            width: "47%",
-            height: "300px",
-            backgroundImage: `url(${PersonalTraining})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            backgroundPosition: "right",
-            
+            display: "flex",
+            width: "100%",
+            height: "250px",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            gap: "30px",
+            backgroundColor: "white",
           }}
-        ></Box></Box>
-          
-          
+        >
+          <Box
+            sx={{
+              width: "21.5%",
+              height: "200px",
+              backgroundColor: "black",
+              padding: "5px",
+              border: "1px solid black",
+              borderRadius: "15px",
+              boxShadow:
+                "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "start",
+              flexDirection: "column",
+              gap: "5px",
+            }}
+          >
+            <Typography
+              sx={{ fontSize: "18px", fontFamily: "Normal", color: "white" }}
+            >
+              {" "}
+              ❝Just wanted to share a quick note and let you know that you guys
+              do a really good job.❞
+            </Typography>
+            <Typography
+              sx={{ fontSize: "18px", fontFamily: "Ubuntu", color: "white" }}
+            >
+              {" "}
+              - James Boyle
+            </Typography>
+            <Typography sx={{ fontSize: "18px", fontFamily: "Ubuntu" }}>
+              {" "}
+              ⭐⭐⭐⭐⭐
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              width: "21.5%",
+              height: "200px",
+              backgroundColor: "black",
+              padding: "5px",
+              border: "1px solid black",
+              borderRadius: "15px",
+              boxShadow:
+                "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "start",
+              flexDirection: "column",
+              gap: "5px",
+            }}
+          >
+            <Typography
+              sx={{ fontSize: "18px", fontFamily: "Normal", color: "white" }}
+            >
+              {" "}
+              ❝Just wanted to share a quick note and let you know that you guys
+              do a really good job.❞
+            </Typography>
+            <Typography
+              sx={{ fontSize: "18px", fontFamily: "Ubuntu", color: "white" }}
+            >
+              {" "}
+              - James Boyle
+            </Typography>
+            <Typography sx={{ fontSize: "18px", fontFamily: "Ubuntu" }}>
+              {" "}
+              ⭐⭐⭐⭐⭐
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              width: "21.5%",
+              height: "200px",
+              backgroundColor: "black",
+              padding: "5px",
+              border: "1px solid black",
+              borderRadius: "15px",
+              boxShadow:
+                "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "start",
+              flexDirection: "column",
+              gap: "5px",
+            }}
+          >
+            <Typography
+              sx={{ fontSize: "18px", fontFamily: "Normal", color: "white" }}
+            >
+              {" "}
+              ❝Just wanted to share a quick note and let you know that you guys
+              do a really good job.❞
+            </Typography>
+            <Typography
+              sx={{ fontSize: "18px", fontFamily: "Ubuntu", color: "white" }}
+            >
+              {" "}
+              - James Boyle
+            </Typography>
+            <Typography sx={{ fontSize: "18px", fontFamily: "Ubuntu" }}>
+              {" "}
+              ⭐⭐⭐⭐⭐
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              width: "21.5%",
+              height: "200px",
+              backgroundColor: "black",
+              padding: "5px",
+              border: "1px solid black",
+              borderRadius: "15px",
+              boxShadow:
+                "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "start",
+              flexDirection: "column",
+              gap: "5px",
+            }}
+          >
+            <Typography
+              sx={{ fontSize: "18px", fontFamily: "Normal", color: "white" }}
+            >
+              {" "}
+              ❝Just wanted to share a quick note and let you know that you guys
+              do a really good job.❞
+            </Typography>
+            <Typography
+              sx={{ fontSize: "18px", fontFamily: "Ubuntu", color: "white" }}
+            >
+              {" "}
+              - James Boyle
+            </Typography>
+            <Typography sx={{ fontSize: "18px", fontFamily: "Ubuntu" }}>
+              {" "}
+              ⭐⭐⭐⭐⭐
+            </Typography>
+          </Box>
+        </Box>
       </Box>
-      <Box    
-        sx={{
-            display: "flex",
-            width: '100%',
-            height: "250px",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-            gap: "30px",
-            backgroundColor: 'white'
-          }}>
-            <Box sx={{width: "48.4%",
-            height: "180px",
-            padding: '10px',
-            backgroundColor: 'white', border: '1px solid black', borderRadius: '15px', boxShadow:"rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",}}>
-            <Typography sx={{fontSize: '18px', fontFamily: "Ubuntu" }}> ❝  Just wanted to share a quick note and let you know that you guys do a really good job. I'm glad I decided to work with you. I never have any problem at all. ❞</Typography>
-            <Typography sx={{fontSize: '18px', fontFamily: "Ubuntu" }}> - James Boyle</Typography>
-            <Typography sx={{fontSize: '18px', fontFamily: "Ubuntu" }}> ⭐⭐⭐⭐⭐</Typography>
-            </Box>
-            <Box sx={{width: "48.4%",
-            height: "180px",
-            padding: '10px',
-            backgroundColor: 'white', border: '1px solid black', borderRadius: '15px', boxShadow:"rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",}}>
-            <Typography sx={{fontSize: '18px', fontFamily: "Ubuntu" }}> ❝  Just wanted to share a quick note and let you know that you guys do a really good job. I'm glad I decided to work with you. I never have any problem at all. ❞</Typography>
-            <Typography sx={{fontSize: '18px', fontFamily: "Ubuntu" }}> - James Boyle</Typography>
-            <Typography sx={{fontSize: '18px', fontFamily: "Ubuntu" }}> ⭐⭐⭐⭐⭐</Typography>
-            </Box>
-          </Box>
-          <Box    
-        sx={{
-            display: "flex",
-            width: '100%',
-            height: "250px",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-            gap: "30px",
-            backgroundColor: 'white'
-          }}>
-            <Box sx={{width: "21.5%",
-            height: "200px",
-            backgroundColor: 'black', padding: '5px', border: '1px solid black', borderRadius: '15px', boxShadow:"rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",}}>
-            <Typography sx={{fontSize: '18px', fontFamily: "Normal", color:'white' }}> ❝Just wanted to share a quick note and let you know that you guys do a really good job.❞</Typography>
-            <Typography sx={{fontSize: '18px', fontFamily: "Ubuntu",  color:'white' }}> - James Boyle</Typography>
-            <Typography sx={{fontSize: '18px', fontFamily: "Ubuntu" }}> ⭐⭐⭐⭐⭐</Typography>
-            </Box>
-            
-            <Box sx={{width: "21.5%",
-            height: "200px",
-            backgroundColor: 'black', padding: '5px', border: '1px solid black', borderRadius: '15px', boxShadow:"rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",}}>
-            <Typography sx={{fontSize: '18px', fontFamily: "Normal", color:'white' }}> ❝Just wanted to share a quick note and let you know that you guys do a really good job.❞</Typography>
-            <Typography sx={{fontSize: '18px', fontFamily: "Ubuntu",  color:'white' }}> - James Boyle</Typography>
-            <Typography sx={{fontSize: '18px', fontFamily: "Ubuntu" }}> ⭐⭐⭐⭐⭐</Typography>
-            </Box>
-            <Box sx={{width: "21.5%",
-            height: "200px",
-            backgroundColor: 'black', padding: '5px', border: '1px solid black', borderRadius: '15px', boxShadow:"rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",}}>
-            <Typography sx={{fontSize: '18px', fontFamily: "Normal", color:'white' }}> ❝Just wanted to share a quick note and let you know that you guys do a really good job.❞</Typography>
-            <Typography sx={{fontSize: '18px', fontFamily: "Ubuntu",  color:'white' }}> - James Boyle</Typography>
-            <Typography sx={{fontSize: '18px', fontFamily: "Ubuntu" }}> ⭐⭐⭐⭐⭐</Typography>
-            </Box>
-            <Box sx={{width: "21.5%",
-            height: "200px",
-            backgroundColor: 'black', padding: '5px', border: '1px solid black', borderRadius: '15px', boxShadow:"rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",}}>
-            <Typography sx={{fontSize: '18px', fontFamily: "Normal", color:'white' }}> ❝Just wanted to share a quick note and let you know that you guys do a really good job.❞</Typography>
-            <Typography sx={{fontSize: '18px', fontFamily: "Ubuntu",  color:'white' }}> - James Boyle</Typography>
-            <Typography sx={{fontSize: '18px', fontFamily: "Ubuntu" }}> ⭐⭐⭐⭐⭐</Typography>
-            </Box>
-          </Box>
-    </Box>
     </Box>
   );
 };
