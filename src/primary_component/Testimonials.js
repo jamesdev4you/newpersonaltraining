f050b4c4aa5d0e070c4acaa5b80590e5a7fcda58
James <jamesdev4you@gmail.com>
2023-06-14 14:51:21 -0400

    testimonials mvp

--- src/primary_component/Testimonials.js
+++ src/primary_component/Testimonials.js
@@ -1,7 +1,295 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Header2 from "../assets/header.jpg";
+import Header from "../secondary_component/Header";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <>
+      <Header />
+      <Box
+        sx={{
+          width: "100%",
+          height: "2000px",
+          backgroundColor: "primary.light",
+          padding: "50px 0px",
+        }}
+      >
+        <Box
+          sx={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            sx={{
+              height: "300px",
+              width: "80%",
+              borderRadius: "15px",
+              backgroundColor: "primary.dark",
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+            }}
+          >
+            <Box
+              sx={{
+                height: "240px",
+                width: "240px",
+                borderRadius: "50%",
+                backgroundImage: `url(${Header2})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "right",
+                border: "5px solid",
+                borderColor: "error.light",
+              }}
+            ></Box>
+            <Box sx={{ width: "70%" }}>
+              <Typography
+                sx={{
+                  fontSize: "24px",
+                  textAlign: "center",
+
+                  color: "white",
+                }}
+              >
+                At RCotterill PT, I want to help men and women that have felt
+                like they've been spinning their wheels with their fitness and
+                health. I grasp that different diets can seem attractive and
+                trying various solutions to find the style of eating that's
+                right for you seems essential - but it can also slow your
+                progress down while you are spending time on trial and error.
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "28px",
+                  textAlign: "right",
+                  paddingTop: "15px",
+                  color: "error.light",
+                }}
+              >
+                - James Boyle
+              </Typography>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              height: "300px",
+              width: "80%",
+              borderRadius: "15px",
+              backgroundColor: "primary.dark",
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+            }}
+          >
+            <Box
+              sx={{
+                height: "240px",
+                width: "240px",
+                borderRadius: "50%",
+                backgroundImage: `url(${Header2})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "right",
+                border: "5px solid",
+                borderColor: "error.light",
+              }}
+            ></Box>
+            <Box sx={{ width: "70%" }}>
+              <Typography
+                sx={{
+                  fontSize: "24px",
+                  textAlign: "center",
+
+                  color: "white",
+                }}
+              >
+                At RCotterill PT, I want to help men and women that have felt
+                like they've been spinning their wheels with their fitness and
+                health. I grasp that different diets can seem attractive and
+                trying various solutions to find the style of eating that's
+                right for you seems essential - but it can also slow your
+                progress down while you are spending time on trial and error.
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "28px",
+                  textAlign: "right",
+                  paddingTop: "15px",
+                  color: "error.light",
+                }}
+              >
+                - James Boyle
+              </Typography>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              height: "300px",
+              width: "80%",
+              borderRadius: "15px",
+              backgroundColor: "primary.dark",
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+            }}
+          >
+            <Box
+              sx={{
+                height: "240px",
+                width: "240px",
+                borderRadius: "50%",
+                backgroundImage: `url(${Header2})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "right",
+                border: "5px solid",
+                borderColor: "error.light",
+              }}
+            ></Box>
+            <Box sx={{ width: "70%" }}>
+              <Typography
+                sx={{
+                  fontSize: "24px",
+                  textAlign: "center",
+
+                  color: "white",
+                }}
+              >
+                At RCotterill PT, I want to help men and women that have felt
+                like they've been spinning their wheels with their fitness and
+                health. I grasp that different diets can seem attractive and
+                trying various solutions to find the style of eating that's
+                right for you seems essential - but it can also slow your
+                progress down while you are spending time on trial and error.
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "28px",
+                  textAlign: "right",
+                  paddingTop: "15px",
+                  color: "error.light",
+                }}
+              >
+                - James Boyle
+              </Typography>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              height: "300px",
+              width: "80%",
+              borderRadius: "15px",
+              backgroundColor: "primary.dark",
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+            }}
+          >
+            <Box
+              sx={{
+                height: "240px",
+                width: "240px",
+                borderRadius: "50%",
+                backgroundImage: `url(${Header2})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "right",
+                border: "5px solid",
+                borderColor: "error.light",
+              }}
+            ></Box>
+            <Box sx={{ width: "70%" }}>
+              <Typography
+                sx={{
+                  fontSize: "24px",
+                  textAlign: "center",
+
+                  color: "white",
+                }}
+              >
+                At RCotterill PT, I want to help men and women that have felt
+                like they've been spinning their wheels with their fitness and
+                health. I grasp that different diets can seem attractive and
+                trying various solutions to find the style of eating that's
+                right for you seems essential - but it can also slow your
+                progress down while you are spending time on trial and error.
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "28px",
+                  textAlign: "right",
+                  paddingTop: "15px",
+                  color: "error.light",
+                }}
+              >
+                - James Boyle
+              </Typography>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              height: "300px",
+              width: "80%",
+              borderRadius: "15px",
+              backgroundColor: "primary.dark",
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+            }}
+          >
+            <Box
+              sx={{
+                height: "240px",
+                width: "240px",
+                borderRadius: "50%",
+                backgroundImage: `url(${Header2})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "right",
+                border: "5px solid",
+                borderColor: "error.light",
+              }}
+            ></Box>
+            <Box sx={{ width: "70%" }}>
+              <Typography
+                sx={{
+                  fontSize: "24px",
+                  textAlign: "center",
+
+                  color: "white",
+                }}
+              >
+                At RCotterill PT, I want to help men and women that have felt
+                like they've been spinning their wheels with their fitness and
+                health. I grasp that different diets can seem attractive and
+                trying various solutions to find the style of eating that's
+                right for you seems essential - but it can also slow your
+                progress down while you are spending time on trial and error.
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "28px",
+                  textAlign: "right",
+                  paddingTop: "15px",
+                  color: "error.light",
+                }}
+              >
+                - James Boyle
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default Testimonials;
